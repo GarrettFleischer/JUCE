599e675b759302c3289e71601b4237b0ed15c275
Garrett Fleischer <garrett.fleischer@yahoo.com>
2022-10-14 01:05:45 -0700

    Update Juce4UnityAudioProcessor.h

--- modules/juce_audio_plugin_client/Unity/Juce4UnityAudioProcessor.h
+++ modules/juce_audio_plugin_client/Unity/Juce4UnityAudioProcessor.h
@@ -7,14 +7,12 @@ namespace juce
 {
     class Juce4UnityAudioProcessor : public AudioProcessor
     {
-    public:
-        Juce4UnityAudioProcessor(const BusesProperties& ioLayouts) : AudioProcessor(ioLayouts){}
-        
-        virtual void loadInstrument(const File& sfzFile) = 0;
-        virtual void unloadInstrument(const String& path) = 0;
+    public:        
+        virtual int loadInstrument(const String& sfzFilePath) = 0;
+        virtual void unloadInstrument(const String& sfzFilePath) = 0;
         virtual void clearInstruments() = 0;
     
-        virtual void setInstrument(const String& path) = 0;
+        virtual void setInstrument(const String& sfzFilePath) = 0;
     
         virtual void noteOn(int channel, int midi, float velocity) = 0;
         virtual void noteOff(int channel, int midi) = 0;
